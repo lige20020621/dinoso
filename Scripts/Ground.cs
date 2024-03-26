a63fe3d790adecfa043d8545773910396d2e2c45
lige20020621 <163881787+lige20020621@users.noreply.github.com>
2024-03-26 10:44:37 +0800

    初始化

---
+++ Scripts/Ground.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Ground : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
+        meshRenderer.material.mainTextureOffset += speed * Time.deltaTime * Vector2.right;
+    }
+
+}
