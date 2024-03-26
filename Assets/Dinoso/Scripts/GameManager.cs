1d425ca69d2d7320bf5c5c0510024f7677960c9e
lige20020621 <163881787+lige20020621@users.noreply.github.com>
2024-03-26 11:44:03 +0800

    reok

---
+++ Assets/Dinoso/Scripts/GameManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager Instance { get; private set; }
+
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeed { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI hiscoreText;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private Button retryButton;
+
+    private Player player;
+    private Spawner spawner;
+
+    private float score;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance ==this )
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+        NewGame();
+    }
+    public void NewGame()
+    {
+        ObstacleDinoso [] obstacles = FindObjectsOfType<ObstacleDinoso >();
+
+        foreach (var obstacleDinoso  in obstacles)
+        {
+            Destroy(obstacleDinoso .gameObject);
+        }
+
+        score = 0f;
+        gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+    }
+
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+
+        UpdateHiscore();
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease * Time.deltaTime;
+        score += gameSpeed * Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt(score).ToString("D5");
+    }
+
+    private void UpdateHiscore()
+    {
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+
+        if (score > hiscore)
+        {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore", hiscore);
+        }
+
+        hiscoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
+    }
+
+}
+    
